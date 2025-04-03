7a8dfbf169b376f038623a3de67fd9ee620e0bfd
hyunmishin <shm2837@gmail.com>
2025-04-03 15:02:33 +0900

    my-diary 토이 플젝 start! - 초기 설정 파일 추가

---
+++ src/test/java/org/example/mydiary/MyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
